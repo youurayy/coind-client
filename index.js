87bcbed3b001a12e2068366846148f1e56f00c04
ypocat <juraj.vitko@gmail.com>
2014-01-26 21:00:05 +0200

    update

---
+++ index.js
@@ -0,0 +1,173 @@
+
+// most of the code taken and adapted from https://github.com/freewil/node-bitcoin
+// changes performed: code updated to follow the Node.js/JavaScript callback/control-flow conventions/style
+
+var laeh2 = require('laeh2');
+var _x = laeh2._x;
+var _e = laeh2._e;
+var async = require('async-mini');
+var utilz = require('utilz');
+var _ = require('underscore');
+var http = require('http');
+var https = require('https');
+
+var Client = function(opts) {
+  this.opts = opts || {};
+  this.http = this.opts.ssl ? https : http;
+};
+
+Client.prototype.cbx = function() {
+  // only return once
+  if(this.returned || !this.callback)
+    return;
+  this.returned = true;
+  this.callback.apply(this, Array.prototype.slice.call(arguments, 0));
+};
+
+Client.prototype.cmd = function(/* method, params, callback */) {
+
+  if(arguments.length < 2)
+    _e('Invalid parameters; expected at least "method/batch" and "callback".');
+
+  var method = arguments[0];
+  var last = arguments.length - 1;
+  this.callback = arguments[last];
+  var cb = this.cbx;
+  var params = [];
+  for(var i = 1; i < last; i++)
+    params.push(arguments[i]);
+
+  var time = Date.now();
+  var requestJSON;
+  var multi;
+
+  if (Array.isArray(method)) {
+    // multiple rpc batch call
+    multi = true;
+    requestJSON = [];
+    method.forEach(function(batchCall, i) {
+      requestJSON.push({
+        id: time + '-' + i,
+        method: batchCall.method,
+        params: batchCall.params
+      });
+    });
+  } else {
+    // single rpc call
+    requestJSON = {
+      id: time,
+      method: method,
+      params: params
+    };
+  }
+
+  // First we encode the request into JSON
+  var requestJSON = JSON.stringify(requestJSON);
+
+  // prepare request options
+  var requestOptions = {
+    host: this.opts.host || 'localhost',
+    port: this.opts.port || 8332,
+    method: 'POST',
+    path: path || '/',
+    headers: {
+      'Host': this.opts.host || 'localhost',
+      'Content-Length': requestJSON.length
+    },
+    agent: this.opts.agent || false,
+    rejectUnauthorized: this.opts.ssl && this.opts.sslStrict !== false
+  };
+
+  if (this.opts.ssl && this.opts.sslCa) {
+    requestOptions.ca = this.opts.sslCa;
+  }
+
+  // use HTTP auth if user and password set
+  if (this.opts.user && this.opts.pass) {
+    requestOptions.auth = this.opts.user + ':' + this.opts.pass;
+  }
+
+  // Now we'll make a request to the server
+  var request = this.http.request(requestOptions);
+
+  request.on('error', cb);
+
+  request.on('response', _x(cb, false, function(response) {
+    // We need to buffer the response chunks in a nonblocking way.
+    var buffer = '';
+    response.on('data', _x(cb, false, function(chunk) {
+      buffer = buffer + chunk;
+    }));
+    // When all the responses are finished, we decode the JSON and
+    // depending on whether it's got a result or an error, we call
+    // emitSuccess or emitError on the promise.
+    response.on('end', _x(cb, false, function() {
+      var err;
+
+      try {
+        var decoded = JSON.parse(buffer);
+      } catch (e) {
+        if (response.statusCode !== 200) {
+          err = new Error('Invalid params, response status code: ' + response.statusCode);
+          err.code = -32602;
+          return cb(err);
+        } else {
+          err = new Error('Problem parsing JSON response from server');
+          err.code = -32603;
+          return cb(err);
+        }
+      }
+
+      if (!Array.isArray(decoded)) {
+        decoded = [decoded];
+      }
+
+      var results = [];
+      var errors = [];
+      var isError = false;
+
+      // iterate over each response, normally there will be just one
+      // unless a batch rpc call response is being processed
+      decoded.forEach(function(decodedResponse, i) {
+        if (decodedResponse.hasOwnProperty('error') && decodedResponse.error != null) {
+          var err = new Error(decodedResponse.error.message || '');
+          if (decodedResponse.error.code) {
+            err.code = decodedResponse.error.code;
+          }
+          errors.push(err);
+          isError = true;
+          results.push(null);
+        } else if (decodedResponse.hasOwnProperty('result')) {
+          errors.push(null);
+          results.push(decodedResponse.result);
+        } else {
+          err = new Error(decodedResponse.error.message || '');
+          if (decodedResponse.error.code) {
+            err.code = decodedResponse.error.code;
+          }
+          errors.push(err);
+          isError = true;
+          results.push(null);
+        }
+      });
+
+      // give 'em the good results too, in case they want to use them
+      if(multi) {
+        cb(isError ? errors : null, results);
+      }
+      else {
+        // handle the case where we can get multi results on a singleton query
+        if(errors.length > 1 || results.length > 1) {
+          cb(isError ? errors : null, results);
+        }
+        else {
+          cb(isError ? errors[0] : null, results[0]);
+        }
+      }
+
+    }));
+  }));
+  request.end(requestJSON);
+};
+
+exports.Client = Client;
---
+++ readme.md
@@ -0,0 +1,86 @@
+
+## coind-client
+
+Client for crypto-currency bitcoind-based daemons.
+
+Code adapted from https://github.com/freewil/node-bitcoin to adhere to the Node.js/JavaScript callback/control-flow conventions/style, and to provide for faster/wrapper-less initialization.
+
+### Usage
+
+```js
+
+var coind = require('coind-client');
+
+var client = new coind.Client({
+  host: 'localhost',
+  port: 8332,
+  user: 'username',
+  pass: 'password'
+});
+
+// single request use:
+
+client.cmd('getbalance', '*', 6, function(err, balance) {
+  if(err)
+    // handle error; e.g. with laeh2
+  console.log('Balance:', balance);
+});
+
+// batch request use:
+
+var batch = [];
+for (var i = 0; i < 10; ++i) {
+  batch.push({
+    method: 'getnewaddress',
+    params: ['myaccount']
+  });
+}
+
+client.cmd(batch, function(errors, addresses) {
+  if(errors)
+    // handle errors; e.g. with laeh2
+  for(var i = 0; i < addresses.length; i++)
+    console.log('Address:', addresses[i]);
+});
+
+```
+
+### Daemon API reference / Available commands
+
+https://en.bitcoin.it/wiki/Original_Bitcoin_client/API_Calls_list
+
+
+### How to install litecoind (or equivalent daemon) (taken from [cryptoengine](https://github.com/Unitech/cryptoengine))
+
+First, the basic install (for ubuntu) :
+
+```bash
+$ sudo apt-get update
+$ sudo apt-get install build-essential libssl-dev libdb5.1-dev libdb5.1++-dev libboost-all-dev git
+$ git clone https://github.com/litecoin-project/litecoin
+$ cd litecoin/src
+$ make -j4 -f makefile.unix USE_UPNP=
+$ ne ~/.litecoind/litecoin.conf # cp what litcoind say
+Then to make litecoind synchronized faster with the network (minutes instead of days) :
+
+$ sudo apt-get install transmission-cli
+$ mkdir dat
+$ transmission-cli http://www.lurkmore.com/litecoin-bootstrap/litecoin-bootstrap.torrent -w dat
+$ xz -d litecoin-bootstrap/bootstrap.dat.xz
+$ cp bootstrap.dat ~/.litecoin/
+```
+
+### How to test locally using Bitcoin-Qt, Litecoin-Qt, etc. (Mac example)
+
+```bash
+/Applications/Bitcoin-Qt.app/Contents/MacOS/Bitcoin-Qt \
+    -server -rpcuser=someusername -rpcpassword=somepasword -rpcport=1234
+```
+
+### TODO
+
+Tests!
+
+### License
+
+MIT
